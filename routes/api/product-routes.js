461af0b8810f8d47d41222134432096d45c32551
Seb <gillisl22@outlook.com>
2022-06-24 19:18:08 -0400

    products api done, hopefully

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -6,13 +6,44 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 // get all products
 router.get('/', (req, res) => {
   // find all products
-  // be sure to include its associated Category and Tag data
+  Product.findAll({
+    attributes: ["id", "product_name", "price", "stock", "category_id"],
+    include: [{
+      model: Category,
+      attributes: ['id', 'category_name']
+    },
+  {
+    model: Tag,
+    attributes: ['id', 'tag_name']
+  }]
+  })
+  .then(dbProduct => res.json(dbProduct))
+  .catch(err => {
+    console.log(err);
+    res.status(500).json(err);
+  })
 });
 
 // get one product
 router.get('/:id', (req, res) => {
   // find a single product by its `id`
-  // be sure to include its associated Category and Tag data
+  Product.findOne({
+    where: {id: req.params.id},
+    attributes: ['id', 'product_name', 'price', 'stock', 'category_id'],
+    include: [{ 
+      model: Category,
+      attributes: ['id', 'category_name']
+  },
+  {
+    model: Tag,
+    attributes: ['id', 'category_name']
+  }]
+})
+.then(dbProduct => res.json(dbProduct))
+.catch(err => {
+  console.log(err);
+  res.status(500).json(err);
+})
 });
 
 // create new product
@@ -91,6 +122,20 @@ router.put('/:id', (req, res) => {
 
 router.delete('/:id', (req, res) => {
   // delete one product by its `id` value
+  product.destory({
+    where: { id: req.params.id},
+  })
+  .then(dbProduct => {
+    if (!dbProduct) {
+      res.status(404).json({ message: "No Product Id's found"});
+      return;
+    }
+    res.json(dbProduct);
+  })
+  .catch(err => {
+    console.log(err);
+    res.status(500).json(err);
+  })
 });
 
 module.exports = router;
